cfa362122f7d8f9bd3d16b684603eb2dd98bc8f5
Masatsugu Shimizu <141806299+koa0930@users.noreply.github.com>
2025-10-11 14:42:25 +0800

    Add files via upload

--- vegalite.js
+++ vegalite.js
@@ -1,1 +1,2 @@
-vegaEmbed("#vis", "js/map.json", {actions:false}).catch(console.error);
+vegaEmbed("#vis-map", "./js/map.json", {actions:false}).catch(console.error);
+vegaEmbed("#vis-task1", "./js/week10.json?v=10", {actions:false}).catch(console.error);

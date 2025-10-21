d8d0f3b18fda85cf9a7bebd66d81c9cd64401482
Masatsugu Shimizu <koage3gemeshigeimi@gmail.com>
2025-10-21 21:32:55 +0800

    check

--- vegalite.js
+++ vegalite.js
@@ -1,2 +1,7 @@
-vegaEmbed("#vis-map", "./map.json", {actions:false}).catch(console.error);
-vegaEmbed("#vis-task1", "./week10.json?v=10", {actions:false}).catch(console.error);
+// Title: Simple Vega-Lite Embeds for 6 Idioms
+vegaEmbed("#vis-map", "./map.json", { actions: false }).catch(console.error);
+vegaEmbed("#vis-bar", "./bar.json", { actions: false }).catch(console.error);
+vegaEmbed("#vis-line", "./line.json", { actions: false }).catch(console.error);
+vegaEmbed("#vis-stacked", "./stacked_area.json", { actions: false }).catch(console.error);
+vegaEmbed("#vis-scatter", "./scatter.json", { actions: false }).catch(console.error);
+vegaEmbed("#vis-donut", "./donut.json", { actions: false }).catch(console.error);

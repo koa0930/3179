2414b3c3cda0c66378b11a0aaa7ba3a07654a72f
Masatsugu Shimizu <koage3gemeshigeimi@gmail.com>
2025-10-21 23:14:35 +0800

    asdasdasd

--- vegalite.js
+++ vegalite.js
@@ -1,4 +1,4 @@
-// Title: Simple Vega-Lite Embeds for 6 Idioms
+
 vegaEmbed("#vis-map", "./map.json", { actions: false }).catch(console.error);
 vegaEmbed("#vis-bar", "./bar.json", { actions: false }).catch(console.error);
 vegaEmbed("#vis-line", "./line.json", { actions: false }).catch(console.error);
